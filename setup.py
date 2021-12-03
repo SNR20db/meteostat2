58c3b56f0edd21770466f597b4a3dd075aa10a4e
Angel <aadrovermartinez@outlook.com>
2021-12-03 15:38:52 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+from os import name, path
+from codecs import open
+
+here=path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
+    long_description=file.read()
+
+for line in open(path.join('','__init__.py')):
+    if line.startswith('__versions__'):
+        exec(line)
+        break
+
+setup(
+    name='meteostat',
+    version=__version__,
+    description='Meteo stat API for python',
+    long_description=long_description,
+    url='https://dev.meteostat.net/',
+    license='MIT',
+    Classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords='meteo meteostat weather weatherAPI meteorology',
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts':[
+            'meteostat=meteostat.__main__:main',
+            'meteo=meteostat.__main__:main'
+        ]
+    },
+    py_modules=['meteostat.meteostat','meteostat.__main__'],
+    test_require=[
+        'pytest'
+    ]
+)
